32ba4ac0d6c281c9d20c81ab084d9593a7b6c59f
RuShakya <shakyarustina012@gmail.com>
2024-10-16 23:54:25 +0545

    Mukherjee JS Third and Fourth Project Done - Fourth API Call - Third Star Rating

---
+++ Mukherjee-Fourth-JS-Project/main.js
@@ -0,0 +1,83 @@
+const postsListContainer = document.querySelector(".posts-list-container");
+
+//fetch using XHR
+// function fetchUsingXHR(){
+//     const xhr = new XMLHttpRequest();
+//     xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+//     xhr.responseType = 'json';
+//     xhr.send();
+//     xhr.onload = () => {
+//         if(xhr.status === 200){
+//             console.log(xhr);
+//             displayResults(xhr.response);
+//         } else{
+//             console.log('Some Error Ocurred.');
+//         }
+//     }
+// }
+
+//fetch using Fetch Method
+// function fetchUsingFetchMethod() {
+//   const fetchRequest = fetch("https://jsonplaceholder.typicode.com/posts", {
+//     method: "GET",
+//   });
+//   fetchRequest.then((response) =>
+//     response
+//       .json()
+//       .then((result) => displayResults(result))
+//       .catch((e) => console.log(e))
+//   );
+// }
+
+//fetch using Async Await Method
+// async function fetchUsingAsyncAwaitMethod(){
+//     const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+//         method: "GET",
+//     });
+
+//     const result = await response.json();
+//     displayResults(result);
+// }
+
+//fetch using XHR and Async Await Method
+function helperMethod(method, url){
+    const promise = new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.responseType = 'json';
+        xhr.send();
+
+        xhr.onload = () => {
+            if(xhr.status === 200){
+                resolve(xhr.response)
+            } else{
+                reject(xhr.response)
+            }
+        }
+    })
+    return promise;
+}
+async function fetchUsingXHRAndAsyncAwaitMethod(){
+    const response = await helperMethod('GET', 'https://jsonplaceholder.typicode.com/posts')
+    // console.log(response);
+    displayResults(response);
+}
+
+
+function displayResults(posts) {
+  postsListContainer.innerHTML = posts
+    .map(
+      (postItem) => `
+        <div class="post-item">
+            <h3>${postItem.title}</h3>
+            <p>${postItem.body}</p>
+        </div>
+    `
+    )
+    .join(" ");
+}
+
+// fetchUsingXHR();
+// fetchUsingFetchMethod();
+// fetchUsingAsyncAwaitMethod();
+fetchUsingXHRAndAsyncAwaitMethod();
